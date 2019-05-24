94b881d27ee9bef66510ebe16b8d6c0d3b889db2
goodbuddyo <sean01234@yahoo.com>
2019-05-24 04:11:43 -0400

    added Foundation gem many foundation features, added service item and service record views to home controller, added bg image, etc, still needs wiring to db, many bugs and display issues etc.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,16 @@ class HomeController < ApplicationController
 
  before_action :authenticate_user!
 
+ def index
+ end
+
+
+ def servicerecords
+ end
+
+
+ def serviceitem
+ end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  
-
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #  root 'home#index'
   # devise instructions say to use this:
   root to: "home#index"
+
+  get '/servicerecords' => 'home#servicerecords'
+
+  get '/serviceitem' => 'home#serviceitem'
 
 end
